96e7de264bd8ac5dfb5adb65d788a2113cc543a5
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-11 21:35:30 +0200

    Make worth adjustable for coins

--- Assets/Scripts/Gameplay/InteractableObjects/Coin.cs
+++ Assets/Scripts/Gameplay/InteractableObjects/Coin.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour, IClickableByPlayer
 {
+	[SerializeField, Min(0)] private int worth = 100;
+	
 	public void OnClickByPlayer(GameObject sender, GameObject receiver)
 	{
 		if(sender.TryGetComponent(out PlayerMoney playerMoney))
 		{
-			playerMoney.ChangeMoneyBy(100);
+			playerMoney.ChangeMoneyBy(worth);
 			Destroy(gameObject);
 		}
 	}
