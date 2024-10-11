a6843126f6ad81b5eff7e18eb12808e8abbb2e99
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-12 01:45:35 +0200

    Add a script for player duck

---
+++ Assets/Scripts/Gameplay/Player/PlayerDuck.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerDuck : MonoBehaviour
+{
+	[SerializeField] private Timer timer;
+	
+	private bool canDuck = true;
+
+	private void Awake()
+	{
+		if(timer != null)
+		{
+			timer.timeElapsedEvent.AddListener(OnTimeElapsed);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if(timer != null)
+		{
+			timer.timeElapsedEvent.RemoveListener(OnTimeElapsed);
+		}
+	}
+
+	private void OnTimeElapsed()
+	{
+		SetDuckState(false);
+	}
+	
+	private void OnDuck(InputValue inputValue)
+	{
+		if(canDuck)
+		{
+			SetDuckState(true);
+		}
+	}
+
+	private void SetDuckState(bool duck)
+	{
+		canDuck = !duck;
+		transform.localScale = LocalScale(duck);
+
+		AdjustPosition(duck);
+		
+		if(duck && timer != null)
+		{
+			//transform.localScale = new Vector3(transform.localScale.x, 0.5f, transform.localScale.z);
+			//canDuck = false;
+
+			//transform.Translate(-Vector2.up*0.25f);
+
+			timer.Reset();
+		}
+	}
+
+	private Vector3 LocalScale(bool duck)
+	{
+		var y = duck ? 0.5f : 1f;
+		
+		return new Vector3(transform.localScale.x, y, transform.localScale.z);
+	}
+
+	private void AdjustPosition(bool duck)
+	{
+		var direction = duck ? Vector2.down : Vector2.up;
+		
+		transform.Translate(direction*0.25f);
+	}
+}
