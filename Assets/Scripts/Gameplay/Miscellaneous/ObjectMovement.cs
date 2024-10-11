5b5fb4fc6fcf926544512da0ef1bded7ac211704
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-11 20:09:35 +0200

    Add moving backward in object movement

--- Assets/Scripts/Gameplay/Miscellaneous/ObjectMovement.cs
+++ Assets/Scripts/Gameplay/Miscellaneous/ObjectMovement.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class ObjectMovement : MonoBehaviour
 {
 	[SerializeField, Min(0f)] private float movementSpeed = 1f;
+	[SerializeField] private bool moveBackward;
 
 	private void Update()
 	{
+		var direction = MovementDirection();
 		var speed = movementSpeed*Time.deltaTime;
 		
-		transform.Translate(speed*Vector2.right);
+		transform.Translate(direction*speed*Vector2.right);
 	}
+
+	private int MovementDirection() => moveBackward ? -1 : 1;
 }
