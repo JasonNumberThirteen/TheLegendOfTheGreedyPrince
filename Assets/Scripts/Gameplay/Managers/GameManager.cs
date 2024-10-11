3c469dda36b086203a4bd1f86d11d7ace4099201
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-11 22:38:33 +0200

    Add a script for game manager

---
+++ Assets/Scripts/Gameplay/Managers/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private PlayerDeath playerDeath;
+
+	private void Awake()
+	{
+		playerDeath = FindObjectOfType<PlayerDeath>();
+
+		if(playerDeath != null)
+		{
+			playerDeath.playerDiedEvent.AddListener(OnPlayerDied);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if(playerDeath != null)
+		{
+			playerDeath.playerDiedEvent.RemoveListener(OnPlayerDied);
+		}
+	}
+
+	private void OnPlayerDied()
+	{
+		DestroyAllComponentsOfType<ObjectMovement>();
+		DestroyAllComponentsOfType<ObjectsSpawner>();
+	}
+
+	private void DestroyAllComponentsOfType<T>() where T : Component
+	{
+		var components = FindObjectsOfType<T>();
+
+		foreach (var component in components)
+		{
+			Destroy(component);
+		}
+	}
+}
