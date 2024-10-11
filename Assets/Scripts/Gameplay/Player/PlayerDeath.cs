09f3e215aabec7f26d8d04aac37ecfdef5ab50e6
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-11 19:17:42 +0200

    Make the player died when all health points are depleted

--- Assets/Scripts/Gameplay/Player/PlayerDeath.cs
+++ Assets/Scripts/Gameplay/Player/PlayerDeath.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerHealth))]
 public class PlayerDeath : MonoBehaviour
 {
-	private void OnTriggerEnter2D(Collider2D collider2D)
+	private PlayerHealth playerHealth;
+
+	private void Awake()
 	{
-		gameObject.SetActive(false);
+		playerHealth = GetComponent<PlayerHealth>();
+
+		playerHealth.playerTookDamageEvent.AddListener(OnPlayerTookDamage);
+	}
+
+	private void OnDestroy()
+	{
+		playerHealth.playerTookDamageEvent.RemoveListener(OnPlayerTookDamage);
+	}
+
+	private void OnPlayerTookDamage(int leftHealthPoints)
+	{
+		if(leftHealthPoints <= 0)
+		{
+			gameObject.SetActive(false);
+		}
 	}
 }
