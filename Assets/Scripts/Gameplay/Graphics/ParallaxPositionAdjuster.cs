93fbdc176bbcd203ffd3cc9f9afb98c4e94f9b81
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-11 20:02:47 +0200

    Move background objects instead of the player

--- Assets/Scripts/Gameplay/Graphics/ParallaxPositionAdjuster.cs
+++ Assets/Scripts/Gameplay/Graphics/ParallaxPositionAdjuster.cs
@@ -2,21 +2,12 @@ using UnityEngine;
 
 public class ParallaxPositionAdjuster : MonoBehaviour
 {
-	[SerializeField] private Camera cameraGO;
-	[SerializeField, Min(0f)] private float cameraPositionMultiplier = 1f;
+	[SerializeField, Min(0f)] private float movementSpeed = 1f;
 
 	private void Update()
 	{
-		if(cameraGO != null)
-		{
-			transform.position = GameObjectPosition();
-		}
-	}
-
-	private Vector3 GameObjectPosition()
-	{
-		var x = cameraGO != null ? cameraGO.transform.position.x*cameraPositionMultiplier : transform.position.x;
-
-		return new Vector3(x, transform.position.y, transform.position.z);
+		var speed = movementSpeed*Time.deltaTime;
+		
+		transform.Translate(speed*Vector2.right);
 	}
 }
