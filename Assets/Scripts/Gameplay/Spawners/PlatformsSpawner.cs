d09c848562e2cc5745d05108eae6f1cb25bd8d1f
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-13 01:40:17 +0200

    Add a script for platforms spawner

---
+++ Assets/Scripts/Gameplay/Spawners/PlatformsSpawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Timer))]
+public class PlatformsSpawner : MonoBehaviour
+{
+	public UnityEvent objectSpawnedEvent;
+	
+	[SerializeField] private GameObject gameObjectPrefab;
+	[SerializeField] private float offsetXPerSpawn = 75f;
+	[SerializeField] private float spawnedObjectsY;
+
+	private Timer timer;
+	private float currentOffsetX;
+
+	private void Awake()
+	{
+		timer = GetComponent<Timer>();
+
+		timer.timeElapsedEvent.AddListener(OnTimeElapsed);
+	}
+
+	private void OnDestroy()
+	{
+		timer.timeElapsedEvent.RemoveListener(OnTimeElapsed);
+	}
+
+	private void OnTimeElapsed()
+	{
+		if(gameObjectPrefab != null)
+		{
+			Instantiate(gameObjectPrefab, SpawnedGOPosition(), Quaternion.identity);
+
+			currentOffsetX += offsetXPerSpawn;
+
+			objectSpawnedEvent?.Invoke();
+		}
+	}
+
+	private Vector2 SpawnedGOPosition()
+	{
+		return new Vector2(currentOffsetX, spawnedObjectsY);
+	}
+}
