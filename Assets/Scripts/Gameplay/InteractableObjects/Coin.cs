480b14c5950b21f5110d6e6f360941ac4e9104c0
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-12 14:27:03 +0200

    Add instantiation of the particle effect when coin is collected

--- Assets/Scripts/Gameplay/InteractableObjects/Coin.cs
+++ Assets/Scripts/Gameplay/InteractableObjects/Coin.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class Coin : MonoBehaviour, IClickableByPlayer
 {
 	[SerializeField, Min(0)] private int worth = 100;
+	[SerializeField] private GameObject particleGO;
 	
 	public void OnClickByPlayer(GameObject sender, GameObject receiver)
 	{
 		if(sender.TryGetComponent(out PlayerMoney playerMoney))
 		{
 			playerMoney.ChangeMoneyBy(worth);
+
+			if(particleGO != null)
+			{
+				Instantiate(particleGO, transform.position, Quaternion.identity);
+			}
+
 			Destroy(gameObject);
 		}
 	}
