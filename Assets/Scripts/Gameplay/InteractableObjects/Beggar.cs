f665c164619c7b8a4accf13d88e27bff9d491e95
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-12 23:16:53 +0200

    Modify implementation in a script for beggar

--- Assets/Scripts/Gameplay/InteractableObjects/Beggar.cs
+++ Assets/Scripts/Gameplay/InteractableObjects/Beggar.cs
@@ -2,25 +2,14 @@ using UnityEngine;
 
 public class Beggar : MonoBehaviour, IClickableByPlayer
 {
-	[SerializeField] private int cost = -30;
-    [SerializeField] private int sympathy = 300;
+	[SerializeField] private int cost = 1;
 	
 	public void OnClickByPlayer(GameObject sender, GameObject receiver)
 	{
-		if(sender.TryGetComponent(out PlayerMoney playerMoney) && sender.TryGetComponent(out PlayerSympathy playerSympathy)
-		&& sender.TryGetComponent(out PlayerHealth playerHealth))
+		if(sender.TryGetComponent(out PlayerMoney playerMoney) && sender.TryGetComponent(out PlayerHealth playerHealth) && playerMoney.GetCurrentMoney() >= cost)
 		{
-			if(playerMoney.GetCurrentMoney() + cost >= 0) {
-                playerMoney.ChangeMoneyBy(cost);
-
-                
-                playerSympathy.ChangeSympathyBy(sympathy);
-                playerHealth.TakeDamage(-1);
-                
-            } 
-            
-			
+			playerMoney.ChangeMoneyBy(cost);
+			playerHealth.HealBy(1);
 		}
-        
 	}
 }
