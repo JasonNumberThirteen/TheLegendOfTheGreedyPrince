89ba2cd406c4f44236006121b3370c34ecc17c06
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-13 08:51:43 +0200

    Make player score increaser giving more points with increasing difficulty

--- Assets/Scripts/Gameplay/Miscellaneous/PlayerScoreIncreaser.cs
+++ Assets/Scripts/Gameplay/Miscellaneous/PlayerScoreIncreaser.cs
@@ -3,21 +3,31 @@ using UnityEngine;
 [RequireComponent(typeof(Timer))]
 public class PlayerScoreIncreaser : MonoBehaviour
 {
-	[SerializeField, Min(1)] private int pointsPerElapsedTime = 10;
+	[SerializeField, Min(1)] private int initialPointsPerElapsedTime = 10;
 	
 	private Timer timer;
 	private PlayerScore playerScore;
 	private PlayerDeath playerDeath;
+	private DifficultyManager difficultyManager;
+
+	private int pointsPerElapsedTime;
 
 	private void Awake()
 	{
 		timer = GetComponent<Timer>();
 		playerScore = FindObjectOfType<PlayerScore>();
 		playerDeath = FindObjectOfType<PlayerDeath>();
+		difficultyManager = FindObjectOfType<DifficultyManager>();
+		pointsPerElapsedTime = initialPointsPerElapsedTime;
 
 		if(playerDeath != null)
 		{
 			playerDeath.playerDiedEvent.AddListener(OnPlayerDied);
+		}
+
+		if(difficultyManager != null)
+		{
+			difficultyManager.difficultyIncreasedEvent.AddListener(OnDifficultyIncreased);
 		}
 
 		timer.timeElapsedEvent.AddListener(OnTimeElapsed);
@@ -30,6 +40,11 @@ public class PlayerScoreIncreaser : MonoBehaviour
 		if(playerDeath != null)
 		{
 			playerDeath.playerDiedEvent.RemoveListener(OnPlayerDied);
+		}
+
+		if(difficultyManager != null)
+		{
+			difficultyManager.difficultyIncreasedEvent.RemoveListener(OnDifficultyIncreased);
 		}
 	}
 
@@ -44,5 +59,10 @@ public class PlayerScoreIncreaser : MonoBehaviour
 	private void OnPlayerDied()
 	{
 		Destroy(gameObject);
+	}
+
+	private void OnDifficultyIncreased(int currentDifficulty)
+	{
+		pointsPerElapsedTime += initialPointsPerElapsedTime;
 	}
 }
