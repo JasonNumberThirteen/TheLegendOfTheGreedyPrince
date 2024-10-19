4a9aad75acf0f116a0493730cf3fa188bba857c7
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-10-19 18:37:45 +0200

    Fix switching music between scenes

--- Assets/Scripts/Menus/MusicManager.cs
+++ Assets/Scripts/Menus/MusicManager.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public class MusicManager : MonoBehaviour
 {
+	[SerializeField] private AudioClip mainMenuMusic;
+	[SerializeField] private AudioClip gameplayMusic;
+	
 	private static MusicManager instance;
+
+	private AudioSource audioSource;
 
 	private void Awake()
 	{
@@ -15,6 +22,39 @@ public class MusicManager : MonoBehaviour
 		else if(instance != null)
 		{
 			Destroy(gameObject);
+		}
+
+		audioSource = GetComponent<AudioSource>();
+
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	private void OnDestroy()
+	{
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+	{
+		switch (scene.name)
+		{
+			case "MainMenuScene":
+				PlayMusic(mainMenuMusic);
+				break;
+
+			case "GameplayScene":
+				PlayMusic(gameplayMusic);
+				break;
+		}
+	}
+
+	private void PlayMusic(AudioClip audioClip)
+	{
+		if(audioClip != null && audioSource.clip != audioClip)
+		{
+			audioSource.clip = audioClip;
+			
+			audioSource.Play();
 		}
 	}
 }
